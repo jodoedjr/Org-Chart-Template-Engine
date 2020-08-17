4ced053fd83b1d0d2d55ade663b900d106035e8e
John Edwards <jdedwards6789@gmail.com>
2020-08-17 15:53:55 -0400

    Functional App. app.js requires clean up and validation. HTML requires bootstrap styling

--- app.js
+++ app.js
@@ -9,11 +9,219 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+const { create } = require("domain");
 
+const managerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the manager's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the manager's ID #?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the manager's email address?"
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is the manager's office number?"
+    }
+];
+const engineerQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the engineer's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the engineer's ID #?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email address?"
+    },
+    {
+        type: "input",
+        name: "github",
+        message: "What is the engineer's github username?"
+    }
+];
+const internQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the intern's name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the intern's ID #?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the intern's email address?"
+    },
+    {
+        type: "input",
+        name: "school",
+        message: "What is the intern's school?"
+    }
+];
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
+async function createManager() {//prompt the user for the manager's information
+    let data = await inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the manager's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the manager's ID #?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the manager's email address?"
+        },
+        {
+            type: "input",
+            name: "officeNumber",
+            message: "What is the manager's office number?"
+        }
+    ]);
+    return new Manager(data.name, data.id, data.email, data.officeNumber);
+}
+async function createEngineer() {//prompt the user for the enginner's information
+    const data = await inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the engineer's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the engineer's ID #?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the engineer's email address?"
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is the engineer's github username?"
+        }
+    ]);
+    return new Engineer(data.name, data.id, data.email, data.github);
+}
+async function createIntern() {//prompt the user for the intern's information
+    const data = await inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the intern's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the intern's ID #?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the intern's email address?"
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What is the intern's school?"
+        }
+    ]);
+    return new Intern(data.name, data.id, data.email, data.school);
+}
 
+async function addEmployee() {
+    const data = await inquirer.prompt([
+        {
+            type: "list",
+            name: "Options",
+            choices: ["Add Engineer", "Add Intern", "Finished - Create Org Chart"]
+        }
+    ]);
+    let employeeHolder = {};
+    switch (data.Options) {
+        case "Add Engineer":
+            employeeHolder = await createEngineer();
+            break;
+        case "Add Intern":
+            employeeHolder = await createIntern();
+            break;
+        default:
+            employeeHolder = await -1;
+    }
+    return employeeHolder;
+}
+
+async function initialize() {
+    const employees = [];
+    let employeeHolder = await createManager();
+    employees.push(employeeHolder);
+    while (employees[employees.length - 1] != -1) {
+        const data = await inquirer.prompt([
+            {
+                type: "list",
+                name: "Options",
+                message: "Add an employee, or exit and create org chart:",
+                choices: ["Add Engineer", "Add Intern", "Finished - Create Org Chart"]
+            }
+        ]);
+        switch (data.Options) {
+            case "Add Engineer":
+                employeeHolder = await createEngineer();
+                break;
+            case "Add Intern":
+                employeeHolder = await createIntern();
+                break;
+            default:
+                employeeHolder = -1;
+        }
+        employees.push(employeeHolder);
+    }
+    employees.pop(); //remove -1 from array
+    const html = render(employees);
+    fs.writeFile(outputPath, html, function(err){
+        if(err){
+            //console.log(err);
+            //return;
+            fs.mkdir(OUTPUT_DIR, 0777, function(err){
+                if(err){
+                    console.log(err);
+                    return;
+                }
+                console.log(`Created "${OUTPUT_DIR}" directory`);
+            });
+        }
+        console.log(`Wrote Org-Chart to "${outputPath}"`);
+    });
+}
+
+initialize();//run program
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
